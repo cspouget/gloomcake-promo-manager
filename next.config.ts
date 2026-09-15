e5e428904c055cf0943801a45213965fe11d77dd
cspouget <ookaydude@yahoo.com>
2026-09-15 01:21:29 -0400

    Enable durable Vercel Workflows

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next';
+import { withWorkflow } from 'workflow/next';
+
+const nextConfig: NextConfig = {
+  experimental: {},
+};
+
+export default withWorkflow(nextConfig);
